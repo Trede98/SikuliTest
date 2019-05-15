76d170fb74f1b2a9f1ef7222cabf3ae7c1475290
Fabio Tredesini <fabiotredesini@hotmail.it>
2019-05-15 12:46:33 +0200

    Creazione

---
+++ SikuliTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SikuliTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SikuliTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c7599971-052e-42fe-8388-ddd919039fb5")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SikuliTest/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sikuli4Net.sikuli_REST;
+using TestStack.White;
+
+namespace SikuliTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Pattern file = new Pattern(@"C:\Users\Fabio\Desktop\jdownloaderImages\file.png");
+            Pattern max = new Pattern(@"C:\Users\Fabio\Desktop\jdownloaderImages\maximize.png");
+            Pattern aggiungi = new Pattern(@"C:\Users\Fabio\Desktop\jdownloaderImages\aggiungi.png");
+            Pattern testoAggiungi = new Pattern(@"C:\Users\Fabio\Desktop\jdownloaderImages\testoAggiungi.png");
+            Pattern continua = new Pattern(@"C:\Users\Fabio\Desktop\jdownloaderImages\continua.png");
+            
+
+            TestStack.White.Application application = TestStack.White.Application.Launch(@"C:\Users\Fabio\AppData\Local\JDownloader v2.0\JDownloader2.exe");
+            
+            Thread.Sleep(5000);
+
+            Sikuli4Net.sikuli_REST.Screen scr = new Sikuli4Net.sikuli_REST.Screen();
+            scr.Click(max);
+            scr.Click(file);
+            scr.Click(aggiungi);
+
+            scr.Click(testoAggiungi);
+            SendKeys.SendWait("http://releases.ubuntu.com/19.04/ubuntu-19.04-desktop-amd64.iso");
+            Thread.Sleep(1000);
+            scr.Click(continua);
+
+
+            Thread.Sleep(4000);
+            application.Close();
+
+        }
+    }
+}
